57c2efb47d31ea116e0a295dc85b1d831ac05c1e
barzankaevMohdan <mohdan.barzankaev@gmail.com>
2022-03-26 17:05:02 +0300

    fix

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
 import developConfig from './develop.config'
-import productionConfig from './develop.config'
+import productionConfig from './production.config'
 
 const isDevelop = process.env.NODE_ENV !== 'production'
 
