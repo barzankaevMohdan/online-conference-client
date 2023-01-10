293cdce2a3d7c5830fa8fa6a00d314f41e68eb72
Mohdan <78851238+barzankaevMohdan@users.noreply.github.com>
2023-01-10 18:30:44 +0300

    Update develop.config.js

--- config/develop.config.js
+++ config/develop.config.js
@@ -1,4 +1,4 @@
 export default {
   BASE_URL: 'https://online-conference-server.onrender.com/api',
-  SOCKET_URL: 'https://mohdan-online-conference-ws.herokuapp.com/',
+  SOCKET_URL: 'https://online-conference-wsserver.onrender.com/',
 }
